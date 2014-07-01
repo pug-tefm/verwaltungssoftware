9b869ae1e51316046fe2c450ff1c57619ac87bb6
Florian <florian.riesch@googlemail.com>
2014-07-01 16:39:30 +0200

    ListView

--- PuG_Verwaltungssoftware/mainWindow.cs
+++ PuG_Verwaltungssoftware/mainWindow.cs
@@ -18,7 +18,8 @@ namespace PuG_Verwaltungssoftware
         private int m = 0;
         private int h = 0;
 
-        bool initKursUebersicht = false;
+        private bool initHome = false;
+        private bool initKursUebersicht = false;
         private BindingSource bindingSourceKursUebersicht = new BindingSource();
 
         public mainWindow()
@@ -222,7 +223,8 @@ namespace PuG_Verwaltungssoftware
 
         private void tabPageHome_Enter(object sender, EventArgs e)
         {
-
+            // Listview-Eintraege hinzufuegen
+            loadList();
         }
 
         private void btMgOeffnen_Click(object sender, EventArgs e)
@@ -326,6 +328,134 @@ namespace PuG_Verwaltungssoftware
 
         }
 
+        // Initialize ListView
+        private void initializeListView()
+        {
+            // Set the view to show details.
+            lvNachrichten.View = View.Details;
 
+            // Allow the user to rearrange columns.
+            lvNachrichten.AllowColumnReorder = true;
+
+            // Select the item and subitems when selection is made.
+            lvNachrichten.FullRowSelect = true;
+
+            // Display grid lines.
+            lvNachrichten.GridLines = false;
+
+            // Sort the items in the list in ascending order.
+            lvNachrichten.Sorting = SortOrder.Ascending;
+
+            // Attach Subitems to the ListView
+            lvNachrichten.Columns.Add("Bezeichnung", 150, HorizontalAlignment.Left);
+            lvNachrichten.Columns.Add("Datum Von", 100, HorizontalAlignment.Left);
+            lvNachrichten.Columns.Add("Datum Bis", 100, HorizontalAlignment.Left);
+            lvNachrichten.Columns.Add("Uhrzeit Von", 100, HorizontalAlignment.Left);
+            lvNachrichten.Columns.Add("Uhrzeit Bis", 100, HorizontalAlignment.Left);
+            lvNachrichten.Columns.Add("Wochentag", 100, HorizontalAlignment.Left);
+        }
+
+        private void loadList()
+        {
+            // Get the table
+            int connected = c.openConnection();
+            if (connected == 0)
+            {
+                if (initHome == false)
+                {
+                    // Listview initialisieren
+                    initializeListView();
+
+                    // Clear the ListView control
+                    lvNachrichten.Items.Clear();
+
+                    DataTable dtable = c.select("SELECT bezeichnung, datum_von, datum_bis, uhrzeit_von, uhrzeit_bis, wochentag FROM kurse;");
+                    c.closeConnection();
+
+                    DateTime today = DateTime.Now;
+
+                    for (int i = 0; i < dtable.Rows.Count; i++) // Alle Zeilen anschauen
+                    {
+                        DateTime beginnDate = DateTime.Parse(dtable.Rows[i]["datum_von"].ToString());
+                        DateTime endDate = DateTime.Parse(dtable.Rows[i]["datum_bis"].ToString());
+                        if (today >= beginnDate && today <= endDate || beginnDate == endDate) // Wenn ein laufender Kurs
+                        {
+                            String stringWochentag = c_Helper.umwandlungIntInWochentag(Convert.ToInt32(dtable.Rows[i]["Wochentag"]));
+                            if (c_Helper.umwandlungWochentagEngToGer(today.DayOfWeek.ToString()) == stringWochentag) // Wenn Tag = Heute
+                            {
+                                DateTime beginnUhrzeit = DateTime.Parse(dtable.Rows[i]["uhrzeit_von"].ToString());
+                                DateTime endUhrzeit = DateTime.Parse(dtable.Rows[i]["uhrzeit_bis"].ToString());
+                                if (today.TimeOfDay > beginnUhrzeit.TimeOfDay && today.TimeOfDay < endUhrzeit.TimeOfDay) // Wenn Uhrzeit passt
+                                {
+
+                                    DataRow drow = dtable.Rows[i];
+
+                                    // Only row that have not been deleted
+                                    if (drow.RowState != DataRowState.Deleted)
+                                    {
+                                        // Define the list items
+                                        ListViewItem lvi = new ListViewItem(drow["bezeichnung"].ToString());
+                                        lvi.SubItems.Add(drow["datum_von"].ToString());
+                                        lvi.SubItems.Add(drow["datum_bis"].ToString());
+                                        lvi.SubItems.Add(drow["uhrzeit_von"].ToString());
+                                        lvi.SubItems.Add(drow["uhrzeit_bis"].ToString());
+                                        lvi.SubItems.Add(drow["wochentag"].ToString());
+                                        lvi.BackColor = Color.Aqua;
+
+                                        // Add the list items to the ListView
+                                        lvNachrichten.Items.Add(lvi);
+                                    }
+                                }
+                                else
+                                {
+                                    DataRow drow = dtable.Rows[i];
+
+                                    // Only row that have not been deleted
+                                    if (drow.RowState != DataRowState.Deleted)
+                                    {
+                                        // Define the list items
+                                        ListViewItem lvi = new ListViewItem(drow["bezeichnung"].ToString());
+                                        lvi.SubItems.Add(drow["datum_von"].ToString());
+                                        lvi.SubItems.Add(drow["datum_bis"].ToString());
+                                        lvi.SubItems.Add(drow["uhrzeit_von"].ToString());
+                                        lvi.SubItems.Add(drow["uhrzeit_bis"].ToString());
+                                        lvi.SubItems.Add(drow["wochentag"].ToString());
+                                        lvi.BackColor = Color.Red;
+
+                                        // Add the list items to the ListView
+                                        lvNachrichten.Items.Add(lvi);
+                                    }
+                                }
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            DataRow drow = dtable.Rows[i];
+
+                            // Only row that have not been deleted
+                            if (drow.RowState != DataRowState.Deleted)
+                            {
+                                // Define the list items
+                                ListViewItem lvi = new ListViewItem(drow["bezeichnung"].ToString());
+                                lvi.SubItems.Add(drow["datum_von"].ToString());
+                                lvi.SubItems.Add(drow["datum_bis"].ToString());
+                                lvi.SubItems.Add(drow["uhrzeit_von"].ToString());
+                                lvi.SubItems.Add(drow["uhrzeit_bis"].ToString());
+                                lvi.SubItems.Add(drow["wochentag"].ToString());
+                                lvi.BackColor = Color.Yellow;
+
+                                // Add the list items to the ListView
+                                lvNachrichten.Items.Add(lvi);
+                            }
+                        }
+                    }
+                    initHome = true;
+                }
+            }
+        }
     }
 }
