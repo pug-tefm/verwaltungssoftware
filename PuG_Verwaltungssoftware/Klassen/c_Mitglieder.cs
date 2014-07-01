16c6ff04b7abd1cc12e421f930c911bd60c428b4
erik <er.ik@hotmail.de>
2014-07-01 10:52:39 +0200

    Mitglieder setter getter

---
+++ PuG_Verwaltungssoftware/Klassen/c_Mitglieder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuG_Verwaltungssoftware.Klassen
+{
+    public class c_Mitglieder : c_Person
+    {
+        // Deklaration
+        private int vertragsId;
+        private int gesperrt;
+        private String kommentar;
+        private String vertragsDatum;
+
+        // Setter
+        public void setVertragsId(int pVertragsId)
+        {
+            this.vertragsId = pVertragsId;
+        }
+
+        public void setGesperrt(int pGesperrt)
+        {
+            this.gesperrt = pGesperrt;
+        }
+
+        public void setKommentar(String pKommentar)
+        {
+            this.kommentar = pKommentar;
+        }
+
+        public void setVertragsDatum(String pVertragsDatum)
+        {
+            this.vertragsDatum = pVertragsDatum;
+        }
+
+        // Getter
+        public int getVertragsId()
+        {
+            return this.vertragsId;
+        }
+
+        public int getGesperrt()
+        {
+            return this.gesperrt;
+        }
+
+        public String getKommentar()
+        {
+            return this.kommentar;
+        }
+
+        public String getVertragsDatum()
+        {
+            return this.vertragsDatum;
+        }
+    }
+}
